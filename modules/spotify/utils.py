346b7ad14256f9d52eec2655f6e85b592860f0e3
Jelly <jellylopster@gmail.com>
2025-12-19 08:54:04 +0100

    SponsorBlock

--- modules/spotify/utils.py
+++ modules/spotify/utils.py
@@ -20,10 +20,20 @@ def get_audio_options():
     opts["outtmpl"] = "storage/temp/%(title)s.%(ext)s"
     opts["postprocessors"] = [
         {
+            'key': 'SponsorBlock',
+            'api': 'https://sponsor.ajay.app',
+            'categories': ['sponsor', 'intro', 'outro', 'selfpromo', 'preview', 'interaction', 'filler'],
+        },
+        {
+            'key': 'ModifyChapters',
+            'remove_sponsor_segments': ['sponsor', 'intro', 'outro', 'selfpromo', 'preview', 'interaction', 'filler']
+        },
+        {
             "key": "FFmpegExtractAudio",
             "preferredcodec": "mp3",
-        }
-    ]
+            "preferredquality": "192",
+        },
+    ],
 
     cookie_file = random_cookie_file("youtube")
     if cookie_file:
